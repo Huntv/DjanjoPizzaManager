8455296287145e282a613a96808b4e3bc64936f1
Huntv <66444942+Huntv@users.noreply.github.com>
2025-02-21 04:36:45 -0700

    Update settings.py

--- DjanjoPizzaManager/settings.py
+++ DjanjoPizzaManager/settings.py
@@ -55,10 +55,6 @@ WSGI_APPLICATION = 'DjanjoPizzaManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'production': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangopizzamanager',
         'USER': 'admin',
