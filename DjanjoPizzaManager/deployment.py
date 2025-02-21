c1a69c7be4b600e8b69bc363bc1e749a924c31cd
Huntv <vincentdang12345@gmail.com>
2025-02-20 22:47:19 -0800

    adding secretKey

--- DjanjoPizzaManager/deployment.py
+++ DjanjoPizzaManager/deployment.py
@@ -4,6 +4,8 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGNS = ['https//' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
