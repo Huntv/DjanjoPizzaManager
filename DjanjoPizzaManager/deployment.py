5aa390ad8cc4f83e4aacc0d6b2e9158e78582d57
Huntv <66444942+Huntv@users.noreply.github.com>
2025-02-21 17:02:33 -0700

    Update deployment.py

--- DjanjoPizzaManager/deployment.py
+++ DjanjoPizzaManager/deployment.py
@@ -1,15 +1,19 @@
-import os 
-from .settings import *
+import os
 from .settings import BASE_DIR
 
-
+# Set the secret key securely from the environment
 SECRET_KEY = os.environ['SECRET']
+
+# Set ALLOWED_HOSTS from the environment variable, default to Azure's hostname if not present
 ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'pizzamanager-ecbha3bfhqf6d3g7.northeurope-01.azurewebsites.net')]
+
+# CSRF_TRUSTED_ORIGINS is required for Azure deployment to prevent CSRF errors
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME', 'pizzamanager-ecbha3bfhqf6d3g7.northeurope-01.azurewebsites.net')]
 
+# Set Debug to True for now (it should be False in production, update accordingly)
 DEBUG = True
 
-# WhiteNoise configuration
+# WhiteNoise configuration to serve static files
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -19,20 +23,22 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-] 
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+# Azure PostgreSQL connection string
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': conn_str_params.get('dbname'),
+        'USER': conn_str_params.get('user'),
+        'PASSWORD': conn_str_params.get('password'),
+        'HOST': conn_str_params.get('host'),
+        'PORT': conn_str_params.get('port', '5432'),
     }
 }
