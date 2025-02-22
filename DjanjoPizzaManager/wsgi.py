d29b1e7ee1aba958ace9e7e56371a47d4f116540
Huntv <vincentdang12345@gmail.com>
2025-02-21 16:44:36 -0800

    Update to wsgi.py

--- DjanjoPizzaManager/wsgi.py
+++ DjanjoPizzaManager/wsgi.py
@@ -1,10 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Set the settings module based on the environment (production or development)
 settings_module = 'DjanjoPizzaManager.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'DjanjoPizzaManager.settings'
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-        settings_module)
+# Set the default settings module if not already set
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
+# Get the WSGI application for Django
 application = get_wsgi_application()
